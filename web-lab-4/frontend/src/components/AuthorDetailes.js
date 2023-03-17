2345a62c5d6e395ce566bfe6482a22c8887f8e9d
Артемий <66246727+Ferotyy@users.noreply.github.com>
2023-03-17 16:16:28 +0300

    Add files via upload

---
+++ web-lab-4/frontend/src/components/AuthorDetailes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthorDetailes = () => {
+  return <div className='detailes'>
+    <label className='author name'>Soloviev Artemiy Aleksandrovich</label><br/>
+    <label className='author group'>P32151</label><br/>
+    <label className='author variant'>Variant: 64312345</label><br/>
+  </div>;
+};
+
+export default AuthorDetailes;
